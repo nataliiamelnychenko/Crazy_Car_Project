c9605583ea7baac5918fcd1875e7046b969cc90a
Nataliia <nataliia.melnychenko@edu.fh-joanneum.at>
2025-10-30 10:50:10 +0100

    Einheit 3 Debug

--- HAL/hal_timerB0.c
+++ HAL/hal_timerB0.c
@@ -7,28 +7,23 @@ void hal_TimerB0Init(void)
 {
     unsigned long f_tick  = (unsigned long)SMCLK_FREQU / (TB0_ID_DIV * TB0_EX_DIV);
     unsigned long ticks   = f_tick / TB0_TOGGLE_FREQ_HZ;
-    if (ticks == 0) ticks = 1;           //страховка
-    unsigned int ccr0_val = (unsigned int)(ticks - 1);   //Up mode рахує
+    if (ticks == 0) ticks = 1;
+    unsigned int ccr0_val = (unsigned int)(ticks - 1);
 
-    //Вибір SMCLK, ID=/8, режим Up, скидання лічильника
     TB0CTL  = TBSSEL_2 | ID_3 | MC_1 | TBCLR;
 
-    // /5
     TB0EX0  = TBIDEX_4;
 
-    //Період і переривання CCR0
     TB0CCR0  = ccr0_val;
-    TB0CCTL0 = CCIE;   //увімкнути переривання CCR0
+
+    TB0CCTL0 = CCIE;
 
     TB0CTL  |= MC__UP;
 }
 
-// Окремий вектор для CCR0:
+
 #pragma vector = TIMERB0_VECTOR
 __interrupt void TB0_CCR0_ISR(void)
 {
-    //перевірити/очистити правильний прапорець
-   {
-        LCD_BL_TOGGLE();        // макрос – вмик./вимик. підсвітку
-    }
+            LCD_BL_TOGGLE();
 }
--- HAL/hal_timerB0.h
+++ HAL/hal_timerB0.h
@@ -5,8 +5,8 @@
 
 #define TB0_TOGGLE_FREQ_HZ  4u
 
-#define TB0_ID_DIV     8u     // ID=/8  (ID_3)
-#define TB0_EX_DIV     5u     // TBIDEX=/5 (TBIDEX_4)
+#define TB0_ID_DIV     8u
+#define TB0_EX_DIV     5u
 
 void hal_TimerB0Init(void);
 
